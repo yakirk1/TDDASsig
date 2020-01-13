f4becb540e59c02a5879010157a169029e27784d
Kobaivanov Yakir <yakirk1@gmail.com>
2020-01-13 15:41:51 +0200

    i

---
+++ src/Bmi.py
@@ -0,0 +1,9 @@
+class BMI:
+
+    @staticmethod
+    #assuming mass is in kg's and height in cms
+    def bmi_calc(height, mass):
+       pass
+
+    def bmi_check(bmi):
+        pass
---
+++ src/BubbleSort.py
@@ -0,0 +1,5 @@
+class BubbleSort:
+
+    @staticmethod
+    def sort(arr):
+        pass
