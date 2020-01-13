8e7f17f881d6863f9dbff288cf8ebed5c36fbd06
Kobaivanov Yakir <yakirk1@gmail.com>
2020-01-13 16:00:42 +0200

    bmi calc&test update

--- src/Bmi.py
+++ src/Bmi.py
@@ -3,7 +3,17 @@ class BMI:
     @staticmethod
     #assuming mass is in kg's and height in cms
     def bmi_calc(height, mass):
-       pass
+        if not isinstance(height, (int, float)) or not isinstance(mass, (int, float)):
+            return "Invalid characters,please type only numerical characters"
+        if height <= 0:
+            return "Invalid height"
+        if mass <= 0:
+            return "Invalid mass"
+
+            # convert cms to ms
+        height = height / 100
+
+        return mass / (height * height)
 
     def bmi_check(bmi):
         pass
--- test/Bmi_test.py
+++ test/Bmi_test.py
@@ -1,10 +1,92 @@
 import unittest
 
+from src.Bmi import BMI
+
 
 class Bmi_test(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_invalid_type(self):
+        # stub
+        height1 = 'a'
+        mass1 = 50
 
+        height2 = 180
+        mass2 = 'abc'
+
+        height3 = "95"
+        mass3 = True
+
+        height4 = 100
+        mass4 = 50
+
+        # assume
+        expected1 = "Invalid characters,please type only numerical characters"
+        expected2 = "Invalid characters,please type only numerical characters"
+        expected3 = "Invalid characters,please type only numerical characters"
+        expected4 = 50
+
+        # action
+        result1 = BMI.bmi_calc(height1, mass1)
+        result2 = BMI.bmi_calc(height2, mass2)
+        result3 = BMI.bmi_calc(height3, mass3)
+        result4 = BMI.bmi_calc(height4, mass4)
+
+        # expect/assert
+        self.assertEqual(result1, expected1)
+        self.assertEqual(result2, expected2)
+        self.assertEqual(result3, expected3)
+        self.assertEqual(result4, expected4)
+
+    def test_divided_by_zero(self):
+        # stub
+        height1 = 0
+        mass1 = 75
+
+        height2 = 0
+        mass2 = 0
+
+        # assume
+        expected1 = "Invalid height"
+        expected2 = "Invalid height"
+
+        # action
+        result1 = BMI.bmi_calc(height1, mass1)
+        result2 = BMI.bmi_calc(height2, mass2)
+
+        # expect/assert
+        self.assertEqual(result1, expected1)
+        self.assertEqual(result2, expected2)
+
+    def test_invalid_values(self):
+        # stub
+        height1 = -10
+        mass1 = 67
+
+        height2 = 180
+        mass2 = 0
+
+        height3 = 59
+        mass3 = -34
+
+        height4 = -158
+        mass4 = -5
+
+        # assume
+        expected1 = "Invalid height"
+        expected2 = "Invalid mass"
+        expected3 = "Invalid mass"
+        expected4 = "Invalid height"
+
+        # action
+        result1 = BMI.bmi_calc(height1, mass1)
+        result2 = BMI.bmi_calc(height2, mass2)
+        result3 = BMI.bmi_calc(height3, mass3)
+        result4 = BMI.bmi_calc(height4, mass4)
+
+        # expect/assert
+        self.assertEqual(result1, expected1)
+        self.assertEqual(result2, expected2)
+        self.assertEqual(result3, expected3)
+        self.assertEqual(result4, expected4)
 
 if __name__ == '__main__':
     unittest.main()
