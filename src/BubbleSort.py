f1bd447aa7803b56b6e247ac92facdb40c3f6db9
Shirel Biton <12shirel@gmail.com>
2020-01-13 17:03:26 +0200

    updating bubblesort.py

--- src/BubbleSort.py
+++ src/BubbleSort.py
@@ -2,4 +2,18 @@ class BubbleSort:
 
     @staticmethod
     def sort(arr):
-        pass
+        if type(arr) is int or type(arr) is float:
+            return arr
+        if type(arr) is str and len(arr) == 1:
+            return arr
+        try:
+            arr = list(arr)
+            n = len(arr)
+            for i in range(n):
+                for j in range(0, n - i - 1):
+                    if arr[j] > arr[j + 1]:
+                        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            return arr
+      
+        except TypeError:
+            return 'Invalid Input'
